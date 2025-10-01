4c1f8153c34260298ed14084cedbc9be76ef907d
adronnie <khadkanabin862@gmail.com>
2025-10-01 11:28:11 +0900

    try

--- src/history.py
+++ src/history.py
@@ -1,24 +1,36 @@
-
-### /src/history.py (starter)
+# /src/history.py
 
 class BrowserHistory:
     def __init__(self, start="home"):
-        # TODO: choose your internal representation (two stacks)
-        self._cur = start
-        self._back = []   # TODO
-        self._fwd = []    # TODO
+        self._start = start      # store start page
+        self._cur = start        # current page
+        self._back = []          # stack for back navigation
+        self._fwd = []           # stack for forward navigation
 
     def visit(self, url: str) -> None:
-        # TODO: push current to back, set current, clear forward
-        raise NotImplementedError
+        """Visit a new URL: push current (if not start page) to back,
+        set new current, and clear forward history."""
+        if self._cur != self._start or self._back:  # don't push 'home' as a history item
+            self._back.append(self._cur)
+        self._cur = url
+        self._fwd.clear()
 
     def back(self) -> str:
-        # TODO: move to previous page; decide error behavior on underflow
-        raise NotImplementedError
+        """Go back to the previous page. Raise IndexError if no history."""
+        if not self._back:
+            raise IndexError("No pages in back history")
+        self._fwd.append(self._cur)
+        self._cur = self._back.pop()
+        return self._cur
 
     def forward(self) -> str:
-        # TODO: move to next page; decide error behavior on underflow
-        raise NotImplementedError
+        """Go forward to the next page. Raise IndexError if no forward history."""
+        if not self._fwd:
+            raise IndexError("No pages in forward history")
+        self._back.append(self._cur)
+        self._cur = self._fwd.pop()
+        return self._cur
 
     def current(self) -> str:
+        """Return the current page."""
         return self._cur
